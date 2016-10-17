5b7c42dd0572ddf0ac824c8a235b4b13bb513655
sanjaygarg0625 <sanjaygarg0625@gmail.com>
2016-10-17 16:21:00 -0400

    Angular JS - People Search

---
+++ AngularJS-PeopleSearch/app/controllers/peopleController.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("peopleSearch")
+        .controller("searchCtrl",
+                     ["$scope", "$http", PeopleSearch]);
+
+    function PeopleSearch($scope, $http) {
+
+        var ppl = this;
+        ppl.searchText = '';
+
+        //  Load JSON Data
+        $http.get('app/data/data.json').success(function (data) {
+            ppl.pplData = data;
+        });
+
+        // Search on text change
+        $scope.getData = function () {
+            filterData();
+        };
+        
+        // Search on click button
+        $scope.applyFilter = function () {
+            filterData();
+        };
+
+        // Filter search data 
+        function filterData() {
+            if (ppl.searchText == '') {
+                ppl.people = null;
+            }
+            else {
+                ppl.people = ppl.pplData;
+            }
+        }
+    }
+
+}());
